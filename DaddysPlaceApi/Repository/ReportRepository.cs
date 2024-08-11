f2d8a0711af827ce012c7b954c4400d278dea998
suneru <suneru89@gmail.com>
2024-08-11 06:38:31 +0530

    Change the Sql Query in report

--- DaddysPlaceApi/Repository/ReportRepository.cs
+++ DaddysPlaceApi/Repository/ReportRepository.cs
@@ -15,7 +15,7 @@ namespace DaddysPlaceApi.Repository
 
         public async Task<IEnumerable<AllBillEntity>> GetDateRangeWiseSales(DateTime StartDate, DateTime EndDate)
         {
-            string sqlString = "SELECT  Bill.Id, Bill.CreateOn, Bill.Frn_UserId,  Bill.TotalAmount As BillTotal,[User].email, PaymentType.PaymentType, (select sum(Bill.TotalAmount) from Bill ) As TotalAmount FROM Bill INNER JOIN PaymentType ON Bill.Frn_PaymentId = PaymentType.Id INNER JOIN [User] ON Bill.Frn_UserId = [User].Id where Bill.CreateOn between @StartDate and @EndDate ";
+            string sqlString = "SELECT  Bill.Frn_OrderId as Id, Bill.CreateOn, Bill.Frn_UserId,  Bill.TotalAmount As BillTotal,[User].email, PaymentType.PaymentType, (select sum(Bill.TotalAmount) from Bill  where Bill.CreateOn between @StartDate and @EndDate ) As TotalAmount FROM Bill INNER JOIN PaymentType ON Bill.Frn_PaymentId = PaymentType.Id INNER JOIN [User] ON Bill.Frn_UserId = [User].Id where Bill.CreateOn between @StartDate and @EndDate ";
             var con = _dbConnectors.CreateConnection();
             var categories = await con.QueryAsync<AllBillEntity>(sqlString, new { StartDate, EndDate });
             return categories;
